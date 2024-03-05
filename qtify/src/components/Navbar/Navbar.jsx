3bf06568afca56eafa24ff149e31d68afb08847b
Souparna Roy <rsouparna@gmail.com>
2024-03-05 14:32:40 +0530

    Added Feedback button

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
 import Searchbar from "../Searchbar/Searchbar";
+import Button from "../Button/Button";
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <Logo />
       <Searchbar />
+      <Button />
     </nav>
   );
 }
