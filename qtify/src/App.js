b607e4b7375de5498c3b3d438fbd176a55757d9d
Souparna Roy <rsouparna@gmail.com>
2024-03-05 14:12:36 +0530

    Added and styled Logo

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 export default function App() {
   return (
     <>
-      <StyledEngineProvider>
+      <StyledEngineProvider injectFirst>
         <Navbar />
       </StyledEngineProvider>
     </>
--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import Logo from "../Logo/Logo";
 
 export default function Navbar() {
-  return <nav className={styles.navbar}></nav>;
+  return (
+    <nav className={styles.navbar}>
+      <Logo />
+    </nav>
+  );
 }
