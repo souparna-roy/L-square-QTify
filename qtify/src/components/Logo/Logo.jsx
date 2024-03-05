1b462257fbb5020ce90e4c1cf816700024404bf9
Souparna Roy <rsouparna@gmail.com>
2024-03-05 16:32:17 +0530

    Fixed CSS

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
+import styles from "../Navbar/Navbar.module.css";
 
 export default function Logo() {
-  return <img src={LogoImage} alt="QTify Logo" width={67} />;
+  return (
+    <img src={LogoImage} alt="QTify Logo" width={67} className={styles.logo} />
+  );
 }
